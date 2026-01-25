40a8329e83a745589c0af8cd9a254d513c61e421
sevinurafni <sevinurafni54@gmail.com>
2026-01-25 20:05:27 +0700

    updat site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Work in Progress';
+export const SITE_DESCRIPTION = 'I learn in public because I forget in private';
